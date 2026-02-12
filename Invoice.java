f4a3cd02421d3374767e227a36f93c8ae9b71b6e
Jala2006 <jsamadova20564@ada.edu.az>
2026-02-12 09:47:03 +0400

    Add files via upload

---
+++ Invoice.java
@@ -0,0 +1,16 @@
+package week03.invoice;
+
+public class Invoice {
+
+    private String partNumber;
+    private String partDescription;
+    private int quantity;
+    private double pricePerItem;
+
+    public Invoice(String partNumber, String partDescription, int quantity, double pricePerItem) {
+        this.partNumber = partNumber;
+        this.partDescription = partDescription;
+        this.quantity = quantity;
+        this.pricePerItem = pricePerItem;
+    }
+}
